cda51d81fe2b0c2fe21165d16e8dcfb993a4311f
Uzen Huang <uzen.huang@gmail.com>
2020-07-15 09:53:16 +0800

    player deck

--- blackjack.c
+++ blackjack.c
@@ -19,12 +19,13 @@ typedef struct Player {
 	int turn;            // played in round or not
 	int count;           // amount in hand without accounting ace
 	int ace;             // amount of ace in hand
+	char deck[2][12];    // 0 for card name, 1 for card value
 } Player;
 
 void newPlayer ( Player * player, int index, char playerName[] );
-int addCard ( Player * player, int index );
-int drawCard ();
+int addCard ( Player * player, int index, int cardNum );
 int addAce ( Player * player, int index );
+void printStatus ( Player * player, int index );
 
 int main () {
 	// Setting up random number generator
@@ -36,6 +37,7 @@ int main () {
 	// Creating Dealer Player
 	newPlayer(player, 0, "dealer");
 
+	int round = 0;
 	// Dealer Loop
 	while ( 1 ) {
 		int dealerCount = addAce(player,0);
@@ -43,40 +45,46 @@ int main () {
 
 		// Dealer Logic
 		if ( dealerCount <= 16 ) 
-			printf("\nYou have drawn a %d\n", addCard(player, 0));
+			printf("\nYou have drawn a %d\n", addCard(player, 0, round));
 		else if ( dealerCount > 21 ) {
-			printf("%d, BUST!", dealerCount);
-			exit(0);
+			printf("%d, BUST!\n", dealerCount);
+			break;
 		}
 		else {
 			printf("Final Value: %d\n", dealerCount);
-			exit(0);
+			break;
 		}
+
+		round++;
 	}
+
+	printStatus();
 }
 
 void newPlayer ( Player * player, int index, char playerName[] ) {
 	player[index].count = 0;
 	player[index].ace = 0;
 	strcpy(player[index].playerName, playerName);
+
+	for (int i = 0; i < 2; i++) 
+		for (int j = 0; j < 12; j++) 
+			player[index].deck[i][j] = 0;
 }
 
-int drawCard () {
+int addCard ( Player * player, int index, int cardNum ) {
 	int selection, i, j;
+
 	do {
-		i = rand() % 13;
-		j = rand() % 4;
+		i = rand() % 4;
+		j = rand() % 13;
 		selection = cards[i][j];
 	} while ( selection == 0 );
 
 	cards[i][j] = 0;
-	return selection;
-}
+	int number = selection;
 
-int addCard ( Player * player, int index ) {
-	int number = drawCard();
-
-	printf("%d",number);
+	player[index].deck[0][cardNum] = i + 1;
+	player[index].deck[1][cardNum] = j + 1;
 	
 	if ( (char) number == 'a' )
 		player[index].ace ++;
@@ -96,4 +104,13 @@ int addAce ( Player * player, int index ) {
 		return count + 10 + aceCount;
 	else
 		return count + aceCount;
+}
+
+void printStatus ( Player * player, int index ) {
+	for (int i = 0; i < 2; i++) {
+		for (int j = 0; j < 12; j++) {
+			printf("%d, ", player[0].deck[i][j]);
+		}
+		printf("\n");
+	}
 }
